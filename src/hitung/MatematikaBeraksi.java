4b1c1a4c2bb16fa55a89111d59fa97ed25a2a2e2
yunimaulina <yunimaulina30@gmail.com>
2023-06-12 15:47:42 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author Yuni maulina1
- */
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+    //membuat objek
+    Matematika yuni = new Matematika (4,5);
     
+    System.out.println("Hasil penjumlahan : "+yuni.setPenjumlahan());
+    System.out.println("Hasil pengurangan : "+yuni.setPengurangan());
+    System.out.println("Hasil perkalian   : "+yuni.setPerkalian());
+    System.out.println("Hasil pembagian   : "+yuni.setPembagian());
+    } 
 }
